d2d32f133a97633c4dab2260fefea1e83ccfe7bd
Shubham shukla <shubhushukla586@gmail.com>
2025-02-16 17:12:23 +0530

    Updatre
    commiy

--- MDU_IITM_Learn/next-sitemap.config.js
+++ MDU_IITM_Learn/next-sitemap.config.js
@@ -1,16 +1,27 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://mduiitmlearn.vercel.app",
-    generateRobotsTxt: true,
-    generateIndexSitemap: false,
-    sitemapSize: 5000,
-    exclude: ["/admin/*", "/dashboard/*"], 
-  
-    additionalPaths: async (config) => [
-      { loc: "/notes", lastmod: new Date().toISOString() },
-      { loc: "/syllabus", lastmod: new Date().toISOString() },
-      { loc: "/cgpa", lastmod: new Date().toISOString() },
-      { loc: "/pyqs", lastmod: new Date().toISOString() },
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://mduitmlearn.vercel.app",
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  sitemapSize: 5000,
+  exclude: ["/admin/*", "/dashboard/*"],
+
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      "https://mduitmlearn.vercel.app/sitemap.xml"
     ],
-  };
-  
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+  },
+
+  additionalPaths: async (config) => [
+    { loc: "/notes", lastmod: new Date().toISOString() },
+    { loc: "/syllabus", lastmod: new Date().toISOString() },
+    { loc: "/cgpa", lastmod: new Date().toISOString() },
+    { loc: "/pyqs", lastmod: new Date().toISOString() },
+  ],
+};
